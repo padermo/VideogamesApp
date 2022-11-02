07f7a304401a4b5fa967ebb86496aa4c6a99be12
padermo <fabioaeh96@gmail.com>
2022-11-02 09:49:51 -0500

    terminado el paginado

--- client/src/components/Videogames/Videogame.jsx
+++ client/src/components/Videogames/Videogame.jsx
@@ -4,7 +4,7 @@ function Videogame({ name, genre, image }) {
   return (
     <div className='container-videogame'>
       <div className="container-img-videogame">
-        <img src={image} alt={`Imagen de ${name}`} />
+        {/* <img src={image} alt={`Imagen de ${name}`} /> */}
       </div>
       <div className="container-info-videogame">
         <h1>{name}</h1>
--- client/src/components/Videogames/Videogames.jsx
+++ client/src/components/Videogames/Videogames.jsx
@@ -7,24 +7,65 @@ function Videogames() {
   let dispatch = useDispatch();
   let state = useSelector(state => state.videogames);
 
+  // ! CREAMOS ESTADO COPIA PAGINADO
+  let itemsPage = 15;
+  const [datos, setDatos] = useState([])
+  const [currentPage, setCurrentPage] = useState(0)
+
+  // cuando se carga el componente despacha la accion y llena el state con todos los datos
   useEffect(() => {
     dispatch(getVideogame());
   }, [dispatch])
+
+  // cuando se llenan todos los datos en el state, llenamos datos
+  useEffect(() => {
+    if (state.length === 1) {
+      setDatos([...state])
+    } else {
+      setDatos([...state].splice(0, 15))
+    }
+  }, [state])
   
-  console.log(state)
+  // nextpage
+  const next = () => {
+    const totalElements = state.length;
+    const nextPage = currentPage + 1;
+    const index = nextPage * itemsPage;
+    if (index >= totalElements) return;
+    setDatos([...state].splice(index, itemsPage));
+    setCurrentPage(nextPage)
+  }
+
+  // prevpage
+  const prev = () => {
+    const prevPage = currentPage - 1;
+    if (prevPage < 0) return;
+    const index = prevPage * itemsPage;
+    setDatos([...state].splice(index, itemsPage));
+    setCurrentPage(prevPage);
+  }
+  
+  console.log(datos)
 
   return (
     <div className='container-videogames'>
       <div className="container-interno-videogames">
-        {
-          state.length ?
-            state.map(e => (
-              <Videogame name={e.name} genre={e.genre} image={e.image} />
-            ))
-            :
-            ""
-        }
-      </div>
+        <div className="container-options-videogames">
+          <button onClick={prev}>prev</button>
+          <button onClick={next}>next</button>
+        </div>
+
+        <div className="container-info-videogames">
+          {
+            datos.length ?
+              datos.map(e => (
+                <Videogame name={e.name} genre={e.genre} image={e.image} />
+              ))
+              :
+              "Cargando datos"
+          }
+        </div>{/* fin container info videogames*/}
+      </div>{/* fin container interno videogames */}
     </div>
   )
 }
