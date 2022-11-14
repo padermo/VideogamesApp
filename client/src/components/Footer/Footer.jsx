ff647a6032bceefce5d218114cdf9ce84e00538d
padermo <fabioaeh96@gmail.com>
2022-11-14 00:23:37 -0500

    componente footer creado

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='container-footer'>
+      <div className="container-interno-footer">
+        <div>
+          Footer Videogames
+        </div>
+        <div>
+          2022
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
