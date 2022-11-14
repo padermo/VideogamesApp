9a2c26b096335f08cd395aa5cbef779e97b89387
padermo <fabioaeh96@gmail.com>
2022-11-14 00:28:43 -0500

    cargando fuentes en index.html y creando hoja de estilos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Home from './components/Home/Home';
+import './styles/style.css'
 
 function App() {
   return (
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,14 +4,16 @@ import Videogames from '../Videogames/Videogames'
 import CreateVideogames from '../Create/CreateVideogames'
 import Header from '../Header/Header';
 import Details from '../Detail/Details';
+import Footer from '../Footer/Footer';
 
 function Home() {
   return (
-    <div>
+    <div className='container-home'>
       <Header/>
       <Route exact path={'/'} render={() => <Videogames />} />
       <Route exact path={'/create-videogames'} render={() => <CreateVideogames />} />
-      <Route exact path={'/detail/:id'} render={({match})=> <Details match={match}/>} />
+      <Route exact path={'/detail/:id'} render={({ match }) => <Details match={match} />} />
+      <Footer/>
     </div>
   )
 }
